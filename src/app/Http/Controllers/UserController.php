3d1148d4236b792d398b5e36839cd33bbdc12a7c
ks24pon <pom123re0624@gmail.com>
2022-01-23 16:27:58 +0900

    修正

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 
-class UesrController extends Controller
+class UserController extends Controller
 {
   // ユーザーページ表示のメソッド
   public function show(string $name)
   {
     $user = User::where('name', $name)->first();
 
-    return view('user.show', [
+    return view('users.show', [
       'user' => $user,
     ]);
   }
