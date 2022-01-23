1e62cac5d7c5f348256fa93f142457c6f28c9ef4
ks24pon <pom123re0624@gmail.com>
2022-01-23 16:12:08 +0900

    ユーザーページの表示作成

--- src/app/Http/Controllers/UesrController.php
+++ src/app/Http/Controllers/UesrController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UesrController extends Controller
 {
-    //
+  // ユーザーページ表示のメソッド
+  public function show(string $name)
+  {
+    $user = User::where('name', $name)->first();
+
+    return view('user.show', [
+      'user' => $user,
+    ]);
+  }
 }
