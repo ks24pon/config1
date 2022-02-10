4f23d1767e97a7da17cd7b3481ed70ff750336c6
watanabereo <reo@watanabereonoMacBook-Air.local>
2022-02-10 18:00:42 +0900

    footer_blade_create

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="page-footer font-small cyan darken-3 mt-5">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12 py-1">
+                <div class="mb-5 flex-center">
+                    <a href="{{ route('register') }}" class="btn btn-outline-white rounded-pill mr-md-3 mr-2">Sign up!</a>
+                    <a href="{{ route('login') }}" class="btn btn-outline-white rounded-pill">Login!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <span class="d-flex justify-content-center flex-wrap text-green small">
+            <address class="small">Copyright &copy; 2022 Reo Watanabe All Rights Reserved.</address>
+    </span>
+</footer>
